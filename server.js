788003dd4d8cea9872eebc48e52ea1fa53081c30
Piyush171104 <141119676+Piyush171104@users.noreply.github.com>
2024-11-30 16:05:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.use(express.static(path.join(__dirname, "frontend")));
 const client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1") 
     .setProject("674ab92e000110d38081") 
-    .setKey("standard_a11c2b401b95a20703ea289981d62eb23225f28b110396ce48d22c6a5ef83ee247941b7583d8a7087052ef714dff173529b8dafc74c9ab417758ce94859336469e50d1121ed8eda1dcaab338478aca88e4fd30cfd0435f875c216444579ccb734d5c7aeba73629b2fdd5da02f7117e45c90aea276116dca5fba9793b1394d945"); // Replace with your API key
-
-const database = new Databases(client);
+    .setKey("standard_a11c2b401b95a20703ea289981d62eb23225f28b110396ce48d22c6a5ef83ee247941b7583d8a7087052ef714dff173529b8dafc74c9ab417758ce94859336469e50d1121ed8eda1dcaab338478aca88e4fd30cfd0435f875c216444579ccb734d5c7aeba73629b2fdd5da02f7117e45c90aea276116dca5fba9793b1394d945"); 
 <<<<<<< HEAD
 const databaseId = "674ab96b003a6b6d83b5"; 
 const collectionId = "674ab9cc00360ba3d51a"; 
